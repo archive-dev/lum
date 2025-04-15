5e570978585099460800851912ff7026d5bf337f
whoisamyy <egegegg002@gmail.com>
2025-04-15 17:00:53 +0300

    fix

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -27,8 +27,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/archive-dev/lum")
             credentials {
-                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
-                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String?
+                username = System.getenv("USERNAME") ?: project.findProperty("gpr.user") as String?
+                password = System.getenv("TOKEN") ?: project.findProperty("gpr.key") as String?
             }
         }
     }
